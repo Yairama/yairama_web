660895b72caa751716aaaa79945439faa33c07cf
Yairama <mcambordam@uni.pe>
2022-10-15 02:45:37 -0500

    Done first 3D steps

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="loli gif" src="./assets/loli-dance.gif">
+  <MainComponent></MainComponent>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import MainComponent from './components/MainComponent.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    MainComponent
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+
+  <h3>Babylon try</h3>
+  <canvas id="MainCanvas"></canvas>
+
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import {BasicScene} from '@/babylon_core/BasicScene';
+
+export default defineComponent({
+  name: 'MainComponent',
+  mounted() {
+    const canvas = document.querySelector("canvas")!;
+    new BasicScene(canvas);
+    console.log("Main component mounted")
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+canvas {
+  width: 70%;
+  height: 70%;
+}
+
+</style>
